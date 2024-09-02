f4be626deef8bae808c1ea54483594a31fa2a8a0
Diocleciano Gonçalves de Oliveira <goncalves@intruser.com.br>
2024-09-02 15:21:35 -0300

    Atualização da tarde

---
+++ desafio_sentenca_bloco.js
@@ -0,0 +1,35 @@
+/*
+    // Bloco 1
+    1
+    3
+    5
+    7
+    9
+
+    // Bloco 2
+    2
+    4
+    6
+    8
+    10
+*/
+
+console.log("Números ímpares");
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+
+}
+
+console.log("Números pares");
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+
+}
