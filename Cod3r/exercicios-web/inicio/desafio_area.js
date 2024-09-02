bde2a8f1617498d3d17142f8f399cfe5e7553d14
Diocleciano Gonçalves de Oliveira <goncalves@intruser.com.br>
2024-09-02 16:19:45 -0300

    Atualização do fim do dia

---
+++ Cod3r/exercicios-web/inicio/desafio_area.js
@@ -0,0 +1,12 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592;
+const raio = 10;
+let area;
+
+area = PI * raio * raio;
+console.log("O valor da área é: " + area + " m2.");
+
+area = Math.PI * raio * raio;
+console.log("O valor da área é: " + area + " m2.");
---
+++ Cod3r/exercicios-web/inicio/desafio_troca.js
@@ -0,0 +1,12 @@
+let a = 7;
+let b = 94;
+
+// c = a;
+// a = b;
+// b = c;
+
+[a, b] = [b, a];
+
+// depois da troca ... a = 94 e b = 7
+console.log(a);
+console.log(b);
