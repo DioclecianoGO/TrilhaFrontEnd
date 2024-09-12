1000ddd95bb7826f4a17439c9ea654c1615bd83c
Diocleciano Gonçalves de Oliveira <goncalves@intruser.com.br>
2024-09-12 15:35:28 -0300

    Atualização - Função: Factory #02

---
+++ Cod3r/exercicios-web/funcao/factory2.js
@@ -0,0 +1,20 @@
+/*function criarProduto (nome, preco){
+    return {
+        nome,
+        preco,
+        desconto: 0.2
+    }
+}
+
+console.log(criarProduto("caneta", 1))
+*/
+function criarProduto(nome, preco) {
+    return {
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto('Notebook', 2199.49))
+console.log(criarProduto('iPad', 1199.49))
