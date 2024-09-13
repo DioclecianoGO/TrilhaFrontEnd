5a5959c39d19b9f95e17fb36a699349db2b765ef
Diocleciano Gonçalves de Oliveira <goncalves@intruser.com.br>
2024-09-13 10:49:30 -0300

    Atualização - Função: Exercicios - Questão #03

---
+++ Cod3r/exercicios-web/funcao/Exercicios/questao03.js
@@ -0,0 +1,8 @@
+// 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function exponenciandoABase(a, b){
+    return console.log(a ** b)
+}
+
+exponenciandoABase(2, 3)
+exponenciandoABase(3,2)
