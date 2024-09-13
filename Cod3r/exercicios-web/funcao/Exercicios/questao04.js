ad719bf711bd46d03da77aa2e5fbac9129570809
Diocleciano Gonçalves de Oliveira <goncalves@intruser.com.br>
2024-09-13 11:05:12 -0300

    Atualização - Função: Exercicios - Questão #04

---
+++ Cod3r/exercicios-web/funcao/Exercicios/questao04.js
@@ -0,0 +1,8 @@
+// 04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão destes dois valores.
+
+function divisao(a,b) {
+    return console.log(`Resultado: ${Math.floor(a / b)} 
+Resto: ${a % b}`)
+}
+
+divisao(7,3)
